b79e116aba5614574fdaed528edd22eb245b6d21
itaybraun1 <90039362+itaybraun1@users.noreply.github.com>
2023-04-16 01:29:18 +0300

    Update list_partitions.sql

--- SQL_Commands/Partitions/list_partitions.sql
+++ SQL_Commands/Partitions/list_partitions.sql
@@ -5,3 +5,21 @@ SELECT pg_inherits.inhparent::regclass AS table_name,
 FROM pg_inherits
 ORDER BY pg_inherits.inhparent, pg_inherits.inhrelid;
 
+--Desc: all the table in the db 
+SELECT nspname AS schema_name, relname AS table_name, * 
+FROM pg_class
+JOIN pg_namespace ON relnamespace = pg_namespace.oid
+WHERE relkind in ('r', 'p')
+AND relispartition = 'false'
+AND nspname not in ('pg_catalog', 'information_schema')
+
+ORDER BY 1, 2
+LIMIT 1000;
+
+--Desc: Tables that have partitions (parent tables) and the number of partitions
+SELECT parent.oid, parent.relname AS table_name, parent.relkind, COUNT(*) AS num_child_tables
+FROM pg_inherits
+JOIN pg_class AS parent ON pg_inherits.inhparent = parent.oid
+WHERE parent.relkind = 'p'
+GROUP BY parent.oid,parent.relname,parent.relkind;
+

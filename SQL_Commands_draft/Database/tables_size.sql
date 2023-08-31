f0442f18e031897e9d7559fccca21f56aa2d156b
itaybraun1 <90039362+itaybraun1@users.noreply.github.com>
2023-08-31 18:15:00 +0300

    Aug 30

--- SQL_Commands_draft/Database/tables_size.sql
+++ SQL_Commands_draft/Database/tables_size.sql
@@ -7,3 +7,10 @@ WHERE
     schemaname NOT LIKE 'pg_%'
 ORDER BY
     pg_total_relation_size(relid) DESC;
+
+-- Table size. Without the usage of pg_stat_statements
+select relname as table_name,
+	pg_table_size(oid) as table_size
+from pg_class
+WHERE relkind = 'r'
+

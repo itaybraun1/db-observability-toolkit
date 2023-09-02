d1db7f5a5bca1e7bf87e774304c1cf6568e6acd6
itaybraun1 <90039362+itaybraun1@users.noreply.github.com>
2023-09-02 19:37:59 +0300

    Buffer Cache

--- SQL_Commands_draft/PG_Server/databases_size.sql
+++ SQL_Commands_draft/PG_Server/databases_size.sql
@@ -1,8 +1,8 @@
 -- Database Size
 SELECT 
-	oid, 
+	oid as db_id, 
   datname as database_name, 
-  pg_database_size(datname) as database_size,
-	pg_size_pretty(pg_database_size(datname))
+  pg_database_size(datname) / 1024 as database_size_kb,
+	pg_size_pretty(pg_database_size(datname)) as db_size
 FROM pg_database 
 WHERE datistemplate = false;

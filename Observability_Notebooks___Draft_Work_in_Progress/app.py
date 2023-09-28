db350aae049269b2d616ca25f6ccd36225652c18
itaybraun1 <90039362+itaybraun1@users.noreply.github.com>
2023-09-28 11:33:39 +0300

    Real-time

---
+++ Observability_Notebooks___Draft_Work_in_Progress/app.py
@@ -0,0 +1,31 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+## The Terminal should show a text similar to this: 
+# Dash is running on http://127.0.0.1:8050/
+
+#  * Serving Flask app 'app'
+# * Debug mode: on
+## (Press CTRL+C to quit)
